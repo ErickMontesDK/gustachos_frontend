43f5d3a848c1d435d24ecb483681d7c69a1892f4
ErickDKnight <erickmontesdk@gmail.com>
2026-02-23 16:03:23 -0800

    feat: init users page

---
+++ src/features/users/components/UsersData.tsx
@@ -0,0 +1,9 @@
+import Layout from "../../../components/Layout";
+
+export default function UsersData() {
+    return (
+        <Layout>
+            <h1>Users Data</h1>
+        </Layout>
+    );
+}
